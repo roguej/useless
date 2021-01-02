5f8ccb0dbae013e6b22c73c80ae806d704c9b69d
Ray Jackson <tuf63500@temple.edu>
2021-01-02 18:15:24 -0500

    Two useless buttons, only one of them works.

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-# Getting Started with Create React App
+# Useless app
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+Totally useless project I'm creating to understand React.
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app). I decided to keep a lot of the boilerplate tutorial stuff here because it might be helpful.
 
 ## Available Scripts
 
@@ -39,7 +41,7 @@ Instead, it will copy all the configuration files and the transitive dependencie
 
 You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
 
-## Learn More
+## Learn More (This section left here because it's kinda useful for me)
 
 You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
 
--- src/useless.js
+++ src/useless.js
@@ -1,17 +1,57 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+         <p>
+          Welcome to my useless React app!
+        </p>
+        <img src={logo} className="App-logo" alt="logo" />
+        <UselessButton type="color"/>
+        
+        <UselessButton text="I do!"/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+const green = '#39D1B4';
+const yellow = '#FFD712';
 
 class UselessButton extends React.Component{
-    changeBackground(){
-
+    constructor(props){
+        super(props);
+        this.state = {color: "not green yet!"};
+        this.showMessage = this.showMessage.bind(this);
+        this.changeColor = this.changeColor.bind(this);
+    }
+    changeColor(){
+        const color = this.state.color === green ? yellow : green;
+        this.setState({color: color});
     }
     showMessage(){
-
+        alert('Told ya!');
     }
     
     render(){
-        return (<button>I do nothing</button>);
+        if(this.props.type === 'message'){
+            return (<button onClick={this.showMessage}>{this.props.text}</button>);
+        }
+        else
+        {
+            return (
+                <div style={{color: this.state.color}}>
+                    <button class="colorButton" onClick={this.changeColor}>{this.props.text}</button>
+                </div>
+            
+            );
+        }
     }
 }
 
-ReactDOM.render(<UselessButton type="colorChange"/>, document.getElementById('app'));
+UselessButton.defaultProps = {text: 'I do not work yet!', type: 'message'};
